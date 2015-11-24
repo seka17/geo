ea01c99833d50a451dadc228dfb42d5762670def
Дмитрий Седых <sedykh@gmail.com>
2015-11-25 00:48:31 +0300

    Комментарии и ожидание прерывания сервиса

--- geo-nats/main.go
+++ geo-nats/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"math"
+	"os"
+	"os/signal"
 	"time"
 
 	"github.com/mdigger/geo"
@@ -11,12 +13,14 @@ import (
 	"github.com/nats-io/nats"
 )
 
+// Названия (subjects) сервисов для NATS.
 const (
 	serviceNameEph = "eph"
 	serviceNameLBS = "lbs"
 )
 
 func main() {
+	// TODO: по-хорошему, нужен, конечно, конфигурационный файл со всеми опциями
 	log.Println("Connecting to NATS...")
 	// подключаемся к NATS-серверу
 	nc, err := nats.DefaultOptions.Connect()
@@ -24,16 +28,19 @@ func main() {
 		log.Println("Error NATS Connect:", err)
 		return
 	}
+	// TODO: добавить encoder сообщений (GOB?)
 	defer nc.Close()
 
 	// загружаем базу данных с гео-информацией по вышкам сотовой связи
-	db, err := lbs.LoadDB("geo.gob")
+	db, err := lbs.LoadDB("cells.gob")
 	if err != nil {
 		log.Println("Error loading GeoDB:", err)
 		return
 	}
 	// добавляем подписку
 	lbsSubs, err := nc.Subscribe(serviceNameLBS, func(msg *nats.Msg) {
+		// пример строки с LBS:
+		// 864078-35827-010003698-fa-2-1e50-772a-95-1e50-773c-a6-1e50-7728-a1-1e50-7725-92-1e50-772d-90-1e50-7741-90-1e50-7726-88
 		req, err := lbs.Parse(string(msg.Data)) // разбираем полученные данные
 		if err != nil {
 			log.Println("Error parse LBS:", err)
@@ -42,6 +49,7 @@ func main() {
 		point := db.Find(req) // получаем точку по координатам
 		if math.IsNaN(point.Lat()) || math.IsNaN(point.Lon()) {
 			log.Println("Error searching LBS:", err)
+			// TODO: наверное, нужно отдавать пустой ответ
 			return
 		}
 		// отправляем ответ с данными
@@ -66,6 +74,7 @@ func main() {
 		data, err := cache.Get(point)              // получаем данные из кеша
 		if err != nil {
 			log.Println("Error Get ephemeridos:", err)
+			// TODO: наверное, нужно отдавать пустой ответ
 			return
 		}
 		// отправляем ответ с данными
@@ -79,15 +88,17 @@ func main() {
 	}
 	defer ephSubs.Unsubscribe()
 
-	// go func() {
-	// 	m, err := nc.Request(serviceNameEph, nil, 50*time.Second)
-	// 	if err != nil {
-	// 		log.Println("Response error:", err)
-	// 		return
-	// 	}
-	// 	log.Println("Response:", string(m.Data))
-	// }()
+	// ждем одного из сигналов...
+	signal := moitorSignals(os.Interrupt, os.Kill)
+	_ = signal
 
-	time.Sleep(time.Minute * 1) // задержка
 	log.Println("Disconnecting from NATS...")
+}
+
+// moitorSignals запускает мониторинг сигналов и возвращает значение, когда получает сигнал.
+// В качестве параметров передается список сигналов, которые нужно отслеживать.
+func moitorSignals(signals ...os.Signal) os.Signal {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, signals...)
+	return <-signalChan
 }
